9fa30e7aee6248471a7ec9e1957ee3daf69f5b25
Matt Sinclair <msincla01@wellington.ks.uiuc.edu>
2020-03-09 10:27:58 -0500

    skeleton code for lipid ion interaction density

---
+++ lipid_ion_density/lipidion.tcl
@@ -0,0 +1,17 @@
+proc alignMembrane {} {
+
+}
+
+proc iterateFrames {} {
+
+}
+
+proc gridSearch {} {
+
+}
+
+source loadtraj_test.tcl
+set nf [molinfo top get numframes]
+set fo [open "iondensity.txt" w]
+
+
