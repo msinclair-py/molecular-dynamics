a4400032eb5e1293b2c2903d7057b7df4eb7a26c
Matt Sinclair <msincla01@wellington.ks.uiuc.edu>
2020-03-10 13:47:44 -0500

    updated memb thickness tcl scripts

--- membrane-thickness/gen_rings.tcl
+++ membrane-thickness/gen_rings.tcl
@@ -1,34 +1,41 @@
-puts "Enter a structure file: "
-gets stdin struc
-puts "\nEnter a trajectory: "
-gets stdin traj
-puts "\nEnter a ring width in A: "
-gets stdin width
+proc getThicc {low high} {
+	set top [atomselect top "(name P and within $high of protein) and (not within $low of protein) and z>0"]
+	set bot [atomselect top "(name P and within $high of protein) and (not within $low of protein) and z<0"]
+	set t [measure center $top]
+	set b [measure center $bot]
+	puts "top: $t\nbot: $b"
 
-#load structure
-mol new $struc
-mol addfile $traj waitfor all
-animate goto end
-
-#get rough average and set number of rings to evaluate
-set rough [atomselect top "name P"]
-$rough writepdb netavg.pdb
-
-set sys [measure minmax $rough]
-set x [lindex [vecsub [lindex $sys 1] [lindex $sys 0]] 0]
-set num [expr {int([expr $x / (2*$width)])} - 2]
-#the -2 in num is arbitrary and just to prevent undersampling of
-#faraway lipids
-
-set ring1 [atomselect top "name P and within $width of protein"]
-$ring1 writepdb ring1.pdb
-
-for {set i 2} {$i < $num} {incr i} {
-   set low [expr (($i -1) * $width)]
-   set high [expr ($i * $width)]
-   set ring [atomselect top "(name P and within $high of protein) and (not within $low of protein)"]
-   $ring writepdb ring$i.pdb
+	set thick [lindex [vecsub [measure center $top] [measure center $bot]] 2]
+	return $thick
 }
 
+
+#load traj and align to protein
+source loadtraj.tcl
+
+set width 3
+set nf [molinfo top get numframes]
+set fo [open "thickness.txt" w]
+
+#set number of rings to evaluate
+set phosphates [atomselect top "name P"]
+
+set sys [measure minmax $phosphates]
+set x [lindex [vecsub [lindex $sys 1] [lindex $sys 0]] 0]
+
+#number of rings should be less than would fit so space uninhabited by lipids isn't sampled
+set num [expr {int([expr $x / (2*$width)])} - 2]
+
+
+#establish ring selections
+for {set fr 0} {$fr < $nf} {incr fr} {
+	animate goto $fr
+	for {set i 1} {$i <= $num} {incr i} {
+		set end [expr {$i * 3}]
+		set start [expr {$end - 3}]
+		set thicc [getThicc $start $end]
+		puts $fo "$fr $i $thicc"
+	}
+}
 
 exit
