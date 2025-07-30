77c4c1b71c899f24cc9f5eb2be36506647dd9c66
msinclair-py <msinclair01940@gmail.com>
2025-07-30 13:00:00 -0500

    actually autodocing?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,17 @@ release = '2025'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown
+# here.
+
+# make sure sphinx always uses the current branch
+import sys
+import molecular_simulations
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon'
