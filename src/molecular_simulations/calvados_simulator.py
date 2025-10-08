67949511c2a08e2936f4ccdf5474925379886b99
Moeen Meigooni <mmeigooni@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-10-08 16:40:54 +0000

    calvados multiresolution simulator

---
+++ src/molecular_simulations/calvados_simulator.py
@@ -0,0 +1,11 @@
+import pip._vendor.tomli as tomllib
+from pathlib import Path
+import os
+from typing import Union, TypeVar
+
+PathLike = Union[Path, str]
+
+class CalvadosSimulator:
+    """
+    """
+    def __init__(self,
