9c32fa97612e26a9108365f9f28db163c35eef38
msinclair-py <msinclair01940@gmail.com>
2025-12-11 09:57:36 -0600

    adding utils for handling amber systems

--- src/molecular_simulations/simulate/__init__.py
+++ src/molecular_simulations/simulate/__init__.py
@@ -1,10 +1,3 @@
 #!/usr/bin/env python
 from .mmpbsa import MMPBSA
 from .omm_simulator import Simulator, ImplicitSimulator, Minimizer
-try:
-    from .parsl_settings import (LocalSettings, 
-                                 WorkstationSettings, 
-                                 PolarisSettings, 
-                                 AuroraSettings)
-except ImportError:
-    pass
